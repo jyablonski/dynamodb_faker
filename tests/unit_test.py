a41a6434cb395f973975e520c00bc0e688f2b57e
jyablonski9 <jyablonski9@gmail.com>
2022-06-12 12:08:23 -0500

    try

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -9,6 +9,7 @@ from src.app import *
 
 # assert that the item was written to dynamodb, and that the hash PK is properly 32 characters long
 def test_write_to_dynamodb(moto_dynamodb, faker, aws_credentials):
+    os.environ['AWS_DEFAULT_REGION'] = 'us-east-2'
     table_name = "jacobs_pytest_table"
     invoke = write_to_dynamodb(faker, table_name)
     table = wr.dynamodb.get_table(table_name=table_name)
